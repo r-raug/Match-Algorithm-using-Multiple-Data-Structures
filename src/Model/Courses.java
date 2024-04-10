ad36281b8dc1703999c20d7c1d3342a7b710ba9d
Diego Ferreira <dlins_ferreira@hotmail.com>
2024-04-10 11:29:31 -0400

    Construtores da classe Course e alguns comentários em JavaDoc

--- src/Model/Courses.java
+++ src/Model/Courses.java
@@ -1,13 +1,44 @@
 package Model;
 
+/**
+ * Represents a course with its attributes.
+ */
 public class Courses {
 
-    private int id;
-    private String title;
-    private String discipline;
-    private short numberOfHours;
-    private short numOfGroups;
+    private int id;                 // The course ID
+    private String title;           // The course title
+    private String discipline;      // The course discipline
+    private short numberOfHours;    // The number of hours for the course
+    private short numOfGroups;      // The number of groups for the course
 
+    /**
+     * Constructs a new Courses object with the specified attributes.
+     *
+     * @param id            The course ID.
+     * @param title         The course title.
+     * @param discipline    The course discipline.
+     * @param numberOfHours The number of hours for the course.
+     * @param numOfGroups   The number of groups for the course.
+     */
+    public Courses(int id, String title, String discipline, short numberOfHours, short numOfGroups){
+        this.id = id;
+        this.title = title;
+        this.discipline = discipline;
+        this.numberOfHours = numberOfHours;
+        this.numOfGroups = numOfGroups;
+    }
 
+    /**
+     * Constructs a new Courses object by copying another Courses object.
+     *
+     * @param CourseToCopy The Courses object to copy.
+     */
+    public Courses(Courses CourseToCopy){
+        this.id = CourseToCopy.id;
+        this.title = CourseToCopy.title;
+        this.discipline = CourseToCopy.discipline;
+        this.numberOfHours = CourseToCopy.numberOfHours;
+        this.numOfGroups = CourseToCopy.numOfGroups;
+    }
 
 }
