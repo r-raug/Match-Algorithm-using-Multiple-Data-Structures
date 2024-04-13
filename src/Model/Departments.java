41f05937cda8c2fb09385ac4a2c55670244d4a97
Diego Ferreira <dlins_ferreira@hotmail.com>
2024-04-13 01:24:49 -0400

    Department class innicializada

--- src/Model/Departments.java
+++ src/Model/Departments.java
@@ -1,5 +1,18 @@
 package Model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Flow.Processor;
+
 public class Departments {
+    private Map<String, Courses> courseMap;
+    private List<Processor> listOfProfs;
+
+    public Departments(List<Processor> listOfProfs) {
+        this.listOfProfs = listOfProfs;
+        this.courseMap = new HashMap<>();
+    }
 
 }
